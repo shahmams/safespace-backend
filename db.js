13190fbed797e19ac137e81089e4f63f197df24c
shahmams <shahmams123@gmail.com>
2026-02-23 13:33:26 +0530

    Fix chat separation and counselling lifecycle

--- db.js
+++ db.js
@@ -1,21 +1,5 @@
-const mysql = require("mysql2/promise");
-const { URL } = require("url");
+const mysql = require("mysql2");
 
-const dbUrl = new URL(process.env.DATABASE_URL);
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
-const pool = mysql.createPool({
-  host: dbUrl.hostname,
-  port: dbUrl.port,
-  user: dbUrl.username,
-  password: dbUrl.password,
-  database: dbUrl.pathname.replace("/", ""),
-
-  ssl: {
-    rejectUnauthorized: false
-  },
-
-  waitForConnections: true,
-  connectionLimit: 10,
-});
-
-module.exports = pool;
+module.exports = pool.promise();
